d70dd033e602037c43a67fa357f38b5d84266842
NikaDeme <nikolozi.demetrashvili.1@btu.edu.ge>
2022-05-19 20:53:55 +0400

    Second Week

--- case2.sql
+++ case2.sql
@@ -1,173 +1,181 @@
--- CREATE TABLE runner_orders(
---    order_id INT,
---    runner_id INT,
---    pickup_time VARCHAR(19),
---    distance VARCHAR(7),
---    duration VARCHAR(10),
---    cancellation VARCHAR(23) 
--- );
+-- Case Study #2 - Pizza Runner
 
--- INSERT INTO runner_orders
---   (order_id, runner_id, pickup_time, distance, duration, cancellation)
--- VALUES
---   ('1', '1', '2020-01-01 18:15:34', '20km', '32 minutes', ''),
---   ('2', '1', '2020-01-01 19:10:54', '20km', '27 minutes', ''),
---   ('3', '1', '2020-01-03 00:12:37', '13.4km', '20 mins', NULL),
---   ('4', '2', '2020-01-04 13:53:03', '23.4', '40', NULL),
---   ('5', '3', '2020-01-08 21:10:57', '10', '15', NULL),
---   ('6', '3', 'null', 'null', 'null', 'Restaurant Cancellation'),
---   ('7', '2', '2020-01-08 21:30:45', '25km', '25mins', 'null'),
---   ('8', '2', '2020-01-10 00:15:02', '23.4 km', '15 minute', 'null'),
---   ('9', '2', 'null', 'null', 'null', 'Customer Cancellation'),
---   ('10', '1', '2020-01-11 18:50:20', '10km', '10minutes', 'null');
+CREATE TABLE runner_orders(
+   order_id INT,
+   runner_id INT,
+   pickup_time VARCHAR(19),
+   distance VARCHAR(7),
+   duration VARCHAR(10),
+   cancellation VARCHAR(23) 
+);
 
--- SELECT * FROM runner_orders;
+INSERT INTO runner_orders
+  (order_id, runner_id, pickup_time, distance, duration, cancellation)
+VALUES
+  ('1', '1', '2020-01-01 18:15:34', '20km', '32 minutes', ''),
+  ('2', '1', '2020-01-01 19:10:54', '20km', '27 minutes', ''),
+  ('3', '1', '2020-01-03 00:12:37', '13.4km', '20 mins', NULL),
+  ('4', '2', '2020-01-04 13:53:03', '23.4', '40', NULL),
+  ('5', '3', '2020-01-08 21:10:57', '10', '15', NULL),
+  ('6', '3', 'null', 'null', 'null', 'Restaurant Cancellation'),
+  ('7', '2', '2020-01-08 21:30:45', '25km', '25mins', 'null'),
+  ('8', '2', '2020-01-10 00:15:02', '23.4 km', '15 minute', 'null'),
+  ('9', '2', 'null', 'null', 'null', 'Customer Cancellation'),
+  ('10', '1', '2020-01-11 18:50:20', '10km', '10minutes', 'null');
 
--- CREATE TABLE runners(
---     runner_id INT,
---     registration_date DATE
--- );
+SELECT * FROM runner_orders;
 
--- INSERT INTO runners(runner_id, registration_date)
--- VALUES
---   (1, '2021-01-01'),
---   (2, '2021-01-03'),
---   (3, '2021-01-08'),
---   (4, '2021-01-15');
+CREATE TABLE runners(
+    runner_id INT,
+    registration_date DATE
+);
 
--- SELECT * FROM runners;
+INSERT INTO runners(runner_id, registration_date)
+VALUES
+  (1, '2021-01-01'),
+  (2, '2021-01-03'),
+  (3, '2021-01-08'),
+  (4, '2021-01-15');
 
--- CREATE TABLE customer_orders(
---     order_id INT,
---     customer_id INT,
---     pizza_id INT,
---     exclusions VARCHAR(4),
---     extras VARCHAR(4),
---     order_date TIMESTAMP
--- );
+SELECT * FROM runners;
 
--- INSERT INTO customer_orders
---   (order_id, customer_id, pizza_id, exclusions, extras, order_date)
--- VALUES
---   ('1', '101', '1', '', '', '2020-01-01 18:05:02'),
---   ('2', '101', '1', '', '', '2020-01-01 19:00:52'),
---   ('3', '102', '1', '', '', '2020-01-02 23:51:23'),
---   ('3', '102', '2', '', NULL, '2020-01-02 23:51:23'),
---   ('4', '103', '1', '4', '', '2020-01-04 13:23:46'),
---   ('4', '103', '1', '4', '', '2020-01-04 13:23:46'),
---   ('4', '103', '2', '4', '', '2020-01-04 13:23:46'),
---   ('5', '104', '1', 'null', '1', '2020-01-08 21:00:29'),
---   ('6', '101', '2', 'null', 'null', '2020-01-08 21:03:13'),
---   ('7', '105', '2', 'null', '1', '2020-01-08 21:20:29'),
---   ('8', '102', '1', 'null', 'null', '2020-01-09 23:54:33'),
---   ('9', '103', '1', '4', '1, 5', '2020-01-10 11:22:59'),
---   ('10', '104', '1', 'null', 'null', '2020-01-11 18:34:49'),
---   ('10', '104', '1', '2, 6', '1, 4', '2020-01-11 18:34:49');
+CREATE TABLE customer_orders(
+    order_id INT,
+    customer_id INT,
+    pizza_id INT,
+    exclusions VARCHAR(4),
+    extras VARCHAR(4),
+    order_date TIMESTAMP
+);
 
--- SELECT * FROM customer_orders;
+INSERT INTO customer_orders
+  (order_id, customer_id, pizza_id, exclusions, extras, order_date)
+VALUES
+  ('1', '101', '1', '', '', '2020-01-01 18:05:02'),
+  ('2', '101', '1', '', '', '2020-01-01 19:00:52'),
+  ('3', '102', '1', '', '', '2020-01-02 23:51:23'),
+  ('3', '102', '2', '', NULL, '2020-01-02 23:51:23'),
+  ('4', '103', '1', '4', '', '2020-01-04 13:23:46'),
+  ('4', '103', '1', '4', '', '2020-01-04 13:23:46'),
+  ('4', '103', '2', '4', '', '2020-01-04 13:23:46'),
+  ('5', '104', '1', 'null', '1', '2020-01-08 21:00:29'),
+  ('6', '101', '2', 'null', 'null', '2020-01-08 21:03:13'),
+  ('7', '105', '2', 'null', '1', '2020-01-08 21:20:29'),
+  ('8', '102', '1', 'null', 'null', '2020-01-09 23:54:33'),
+  ('9', '103', '1', '4', '1, 5', '2020-01-10 11:22:59'),
+  ('10', '104', '1', 'null', 'null', '2020-01-11 18:34:49'),
+  ('10', '104', '1', '2, 6', '1, 4', '2020-01-11 18:34:49');
 
--- CREATE TABLE pizza_names(
---     pizza_id INT,
---     pizza_name TEXT
--- );
+SELECT * FROM customer_orders;
 
--- INSERT INTO pizza_names
---   (pizza_id, pizza_name)
--- VALUES
---   (1, 'Meatlovers'),
---   (2, 'Vegetarian');
+CREATE TABLE pizza_names(
+    pizza_id INT,
+    pizza_name TEXT
+);
 
--- SELECT * FROM pizza_names;
+INSERT INTO pizza_names
+  (pizza_id, pizza_name)
+VALUES
+  (1, 'Meatlovers'),
+  (2, 'Vegetarian');
 
--- CREATE TABLE pizza_recipes(
---     pizza_id INT,
---     toppings TEXT
--- );
+SELECT * FROM pizza_names;
 
--- INSERT INTO pizza_recipes
---   (pizza_id, toppings)
--- VALUES
---   (1, '1, 2, 3, 4, 5, 6, 8, 10'),
---   (2, '4, 6, 7, 9, 11, 12');
+CREATE TABLE pizza_recipes(
+    pizza_id INT,
+    toppings TEXT
+);
 
--- SELECT * FROM pizza_recipes;  
+INSERT INTO pizza_recipes
+  (pizza_id, toppings)
+VALUES
+  (1, '1, 2, 3, 4, 5, 6, 8, 10'),
+  (2, '4, 6, 7, 9, 11, 12');
 
--- CREATE TABLE pizza_toppings(
---     topping_id INT,
---     topping_name TEXT
--- );
+SELECT * FROM pizza_recipes;  
 
--- INSERT INTO pizza_toppings
---   (topping_id, topping_name)
--- VALUES
---   (1, 'Bacon'),
---   (2, 'BBQ Sauce'),
---   (3, 'Beef'),
---   (4, 'Cheese'),
---   (5, 'Chicken'),
---   (6, 'Mushrooms'),
---   (7, 'Onions'),
---   (8, 'Pepperoni'),
---   (9, 'Peppers'),
---   (10, 'Salami'),
---   (11, 'Tomatoes'),
---   (12, 'Tomato Sauce');
+CREATE TABLE pizza_toppings(
+    topping_id INT,
+    topping_name TEXT
+);
 
--- SELECT * FROM pizza_toppings;
+INSERT INTO pizza_toppings
+  (topping_id, topping_name)
+VALUES
+  (1, 'Bacon'),
+  (2, 'BBQ Sauce'),
+  (3, 'Beef'),
+  (4, 'Cheese'),
+  (5, 'Chicken'),
+  (6, 'Mushrooms'),
+  (7, 'Onions'),
+  (8, 'Pepperoni'),
+  (9, 'Peppers'),
+  (10, 'Salami'),
+  (11, 'Tomatoes'),
+  (12, 'Tomato Sauce');
+
+SELECT * FROM pizza_toppings;
 
 
--- 1. How many pizzas were ordered?
--- SELECT COUNT(*) AS Pizzas_Ordered
--- FROM customer_orders;
+-- How many pizzas were ordered?
 
--- 2. How many unique customer orders were made?
--- SELECT COUNT(DISTINCT order_id)
--- FROM customer_orders;
+SELECT COUNT(*)
+FROM customer_orders;
 
--- 3. How many successful orders were delivered by each runner?
--- SELECT runner_id, COUNT(order_id)
--- FROM runner_orders
--- WHERE pickup_time != 'null'
--- GROUP BY runner_id;
+-- How many unique customer orders were made?
 
--- 4. How many of each type of pizza was delivered?
--- SELECT p.pizza_name, COUNT(c.pizza_id) AS Pizza_Delivered
--- FROM customer_orders AS c
--- INNER JOIN pizza_names AS p ON c.pizza_id = p.pizza_id
--- INNER JOIN runner_orders as r ON r.order_id = c.order_id
--- WHERE pickup_time != 'null'
--- GROUP BY p.pizza_name;
+SELECT COUNT(DISTINCT order_id)
+FROM customer_orders;
 
--- 5. How many Vegetarian and Meatlovers were ordered by each customer?
--- SELECT c.customer_id, p.pizza_name, COUNT(p.pizza_name)
--- FROM customer_orders as 
--- INNER JOIN pizza_names AS p ON c.pizza_id = p.pizza_id
--- GROUP BY c.customer_id, p.pizza_name;
+-- How many successful orders were delivered by each runner?
 
--- 6. What was the maximum number of pizzas delivered in a single order?
+SELECT runner_id, COUNT(order_id)
+FROM runner_orders
+WHERE pickup_time != 'null'
+GROUP BY runner_id;
 
--- WITH Pizza AS(
---     SELECT c.order_id, COUNT(c.pizza_id) AS Pizza_Ordered
---     FROM customer_orders as c
---     INNER JOIN runner_orders as r ON c.order_id = r.order_id
---     WHERE r.pickup_time != 'null'
---     GROUP BY c.order_id
--- )
+-- How many of each type of pizza was delivered?
 
--- SELECT MAX(Pizza_Ordered)
--- FROM Pizza;
+SELECT p.pizza_name, COUNT(c.pizza_id) AS Pizza_Delivered
+FROM customer_orders AS c
+INNER JOIN pizza_names AS p ON c.pizza_id = p.pizza_id
+INNER JOIN runner_orders as r ON r.order_id = c.order_id
+WHERE pickup_time != 'null'
+GROUP BY p.pizza_name;
 
--- 7. For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
--- SELECT c.customer_id,
--- SUM(
---     CASE WHEN c.extras = '' OR c.exclusions = '' THEN 1
---     ELSE 0
---     END) AS One_Change
--- SUM(
---     CASE WHEN c.extras = '' AND c.exclusions = '' THEN 1
---     ELSE 0
---     END) AS No_Change
--- FROM customer_orders as c 
--- INNER JOIN runner_orders as r ON c.order_id = r.order_id
--- WHERE r.pickup_time != 'null'
--- GROUP BY c.customer_id;
+-- How many Vegetarian and Meatlovers were ordered by each customer?
+
+SELECT c.customer_id, p.pizza_name, COUNT(p.pizza_name)
+FROM customer_orders as 
+INNER JOIN pizza_names AS p ON c.pizza_id = p.pizza_id
+GROUP BY c.customer_id, p.pizza_name;
+
+-- What was the maximum number of pizzas delivered in a single order?
+
+WITH Pizza AS(
+    SELECT c.order_id, COUNT(c.pizza_id) AS Pizza_Ordered
+    FROM customer_orders as c
+    INNER JOIN runner_orders as r ON c.order_id = r.order_id
+    WHERE r.pickup_time != 'null'
+    GROUP BY c.order_id
+)
+
+SELECT MAX(Pizza_Ordered)
+FROM Pizza;
+
+-- For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+
+SELECT c.customer_id,
+SUM(
+    CASE WHEN c.extras = '' OR c.exclusions = '' THEN 1
+    ELSE 0
+    END) AS One_Change
+SUM(
+    CASE WHEN c.extras = '' AND c.exclusions = '' THEN 1
+    ELSE 0
+    END) AS No_Change
+FROM customer_orders as c 
+INNER JOIN runner_orders as r ON c.order_id = r.order_id
+WHERE r.pickup_time != 'null'
+GROUP BY c.customer_id;
